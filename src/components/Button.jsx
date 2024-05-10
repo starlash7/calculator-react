fab38042b298fa400e9d9cd9f66fae9b12b69620
starlash7 <yang92sw@gmail.com>
2024-05-10 15:31:44 +0900

    ButtonNumber

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,12 +1,4 @@
-const Button = ({ name, result, setResult }) => {
-  const onClickButton = () => {
-    if (result === "0") {
-      setResult(name);
-    } else {
-      setResult(result + name);
-    }
-  };
-
+const Button = ({ name, onClickButton }) => {
   return (
     <button
       className="bg-gray-200 w-10 h-10 rounded-md flex justify-center items-center shadow-sm"
--- src/components/Calculator.jsx
+++ src/components/Calculator.jsx
@@ -1,6 +1,8 @@
 import { useState } from "react";
+
 import Button from "./Button";
 import Result from "./Result";
+import ButtonNumber from "./ButtonNumber";
 
 const Calculator = () => {
   const [result, setResult] = useState("0");
@@ -9,20 +11,20 @@ const Calculator = () => {
     <div className="bg-gray-300 p-4 shadow-md">
       <Result result={result} />
       <div className="grid grid-cols-4 gap-2 mt-4">
-        <Button name={"1"} result={result} setResult={setResult} />
-        <Button name={"2"} result={result} setResult={setResult} />
-        <Button name={"3"} result={result} setResult={setResult} />
+        <ButtonNumber name="1" result={result} setResult={setResult} />
+        <ButtonNumber name="2" result={result} setResult={setResult} />
+        <ButtonNumber name="3" result={result} setResult={setResult} />
         <Button name="+" />
-        <Button name={"4"} result={result} setResult={setResult} />
-        <Button name={"5"} result={result} setResult={setResult} />
-        <Button name={"6"} result={result} setResult={setResult} />
+        <ButtonNumber name="4" result={result} setResult={setResult} />
+        <ButtonNumber name="5" result={result} setResult={setResult} />
+        <ButtonNumber name="6" result={result} setResult={setResult} />
         <Button name="-" />
-        <Button name={"7"} result={result} setResult={setResult} />
-        <Button name={"8"} result={result} setResult={setResult} />
-        <Button name={"9"} result={result} setResult={setResult} />
+        <ButtonNumber name="7" result={result} setResult={setResult} />
+        <ButtonNumber name="8" result={result} setResult={setResult} />
+        <ButtonNumber name="9" result={result} setResult={setResult} />
         <Button name="✕" />
         <Button name="Enter" />
-        <Button name={"0"} result={result} setResult={setResult} />
+        <ButtonNumber name="0" result={result} setResult={setResult} />
         <Button name="Del" />
         <Button name="%" />
       </div>
